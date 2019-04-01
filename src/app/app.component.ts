05a5c6217540c9bcc3b33fc6efbb68619b3b1ede
William Ghelfi <trumbitta@gmail.com>
2019-04-01 14:11:19 +0200

    feat(persons): Get data from a service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,19 @@ import { Component } from '@angular/core';
 // App Models
 import { Person } from './persons/person.model';
 
+// App Services
+import { PersonsService } from './persons/persons.service';
+
 @Component({
   selector: 'ng4d-root',
   template: `
-    <ng4d-greeting-card [person]="person1"></ng4d-greeting-card>
-    <ng4d-greeting-card [person]="person2"></ng4d-greeting-card>
-    <ng4d-greeting-card [person]="person3"></ng4d-greeting-card>
+    <ng4d-greeting-card *ngFor="let person of persons" [person]="person"></ng4d-greeting-card>
   `,
-  styles: [],
 })
 export class AppComponent {
-  person1 = new Person({ name: 'William', age: 41 });
-  person2 = new Person({ name: 'Mickey', age: 90 });
-  person3 = new Person({ name: 'Donald', age: 85 });
+  persons: Person[];
+
+  constructor(private personsService: PersonsService) {
+    this.persons = this.personsService.getPersons();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// App Components
 import { AppComponent } from './app.component';
 import { GreetingCardComponent } from './greeting-card/greeting-card.component';
 
+// App Modules
+import { PersonsModule } from './persons/persons.module';
+
 @NgModule({
   declarations: [AppComponent, GreetingCardComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, PersonsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
